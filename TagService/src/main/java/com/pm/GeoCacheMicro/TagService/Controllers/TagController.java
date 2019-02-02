e3223fa1137c3002f4ab04f4d5161f3a06fc3294
pmac1991 <piomac11@gmail.com>
2019-02-02 18:11:49 +0100

    Gateway service initial commit

--- TagService/src/main/java/com/pm/GeoCacheMicro/TagService/Controllers/TagController.java
+++ TagService/src/main/java/com/pm/GeoCacheMicro/TagService/Controllers/TagController.java
@@ -1,4 +1,52 @@
 package com.pm.GeoCacheMicro.TagService.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/")
 public class TagController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/")
+    public String home() {
+        // This is useful for debugging
+        // When having multiple instance of gallery service running at different ports.
+        // We load balance among them, and display which instance received the request.
+        return "Hello from Tag Service running at port: " + env.getProperty("local.server.port");
+    }
+
+    @RequestMapping("/{id}")
+    public String getTag(@PathVariable final int id) {
+        /*// create gallery object
+        Gallery gallery = new Gallery();
+        gallery.setId(id);
+
+        // get list of available images
+        List<Object> images = restTemplate.getForObject("http://image-service/images/", List.class);
+        gallery.setImages(images);*/
+
+        return "test";
+    }
+
+    // -------- Admin Area --------
+    // This method should only be accessed by users with role of 'admin'
+    // We'll add the logic of role based auth later
+    @RequestMapping("/admin")
+    public String homeAdmin() {
+        return "This is the admin area of Tag service running at port: " + env.getProperty("local.server.port");
+    }
+
+
 }
+
+

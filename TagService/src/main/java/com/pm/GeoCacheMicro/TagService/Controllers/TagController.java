f118c32ae8e4d36953751ef65a7171af1514a171
pmac1991 <piomac11@gmail.com>
2019-02-02 17:42:50 +0100

    Tag service initial commit

---
+++ TagService/src/main/java/com/pm/GeoCacheMicro/TagService/Controllers/TagController.java
@@ -0,0 +1,4 @@
+package com.pm.GeoCacheMicro.TagService.Controllers;
+
+public class TagController {
+}
---
+++ TagService/src/main/java/com/pm/GeoCacheMicro/TagService/TagServiceApplication.java
@@ -0,0 +1,34 @@
+package com.pm.GeoCacheMicro.TagService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class TagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagServiceApplication.class, args);
+	}
+
+
+	@Configuration
+	class RestTemplateConfig {
+
+		// Create a bean for restTemplate to call services
+		@Bean
+		@LoadBalanced        // Load balance between service instances running at different ports.
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+	}
+
+}
+
+

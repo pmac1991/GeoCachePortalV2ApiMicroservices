fbf3f512bcf73b3fb68392f95e9691621ac80a58
pmac1991 <piomac11@gmail.com>
2019-02-02 13:02:24 +0100

    Initial commit of Eureka service

---
+++ ServiceRegistryEureka/src/test/java/com/pm/GeoCacheMicro/ServiceRegistryEureka/ServiceRegistryEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pm.GeoCacheMicro.ServiceRegistryEureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceRegistryEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
